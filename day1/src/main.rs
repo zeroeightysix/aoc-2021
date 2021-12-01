414fff83e07efef9b9dbb3be0765fd6b4256e012
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2021-12-01 11:21:38 +0100

    Day 1 Part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,16 +1,20 @@
 const INPUT: &'static str = include_str!("input");
 
 fn main() {
-    let mut lines = INPUT.lines()
-        .map(|line| line.parse::<usize>().unwrap());
-    let mut v = lines.next().unwrap();
+    let lines: Vec<usize> = INPUT
+        .lines()
+        .map(|v| v.parse::<usize>().unwrap())
+        .collect();
+
+    let mut v: usize = lines[0..=2].iter().sum();
     let mut increases = 0;
-    for line in lines {
-        if line > v {
+    for y in 0..lines.len()-2 {
+        let sum: usize = lines[y..=y+2].iter().sum();
+        if sum > v {
             increases += 1;
         }
-        v = line;
+        v = sum;
     }
 
-    println!("{} increases", increases);
+    println!("{}", increases);
 }
