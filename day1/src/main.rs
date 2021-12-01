79e36e25af36ae64d607fec23fe72a2591435251
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2021-12-01 11:09:41 +0100

    Day 1 Part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,16 @@
+const INPUT: &'static str = include_str!("input");
+
 fn main() {
-    println!("Hello, world!");
+    let mut lines = INPUT.lines()
+        .map(|line| line.parse::<usize>().unwrap());
+    let mut v = lines.next().unwrap();
+    let mut increases = 0;
+    for line in lines {
+        if line > v {
+            increases += 1;
+        }
+        v = line;
+    }
+
+    println!("{} increases", increases);
 }
