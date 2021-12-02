883154e0e836e7a5ca1a01ba5d2d24bdc141f4bc
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2021-12-02 13:28:31 +0100

    Day 2 Part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -17,3 +17,21 @@ fn main() {
 
     dbg!(x * y);
 }
+
+#[cfg(not(feature = "part1"))]
+fn main() {
+    let (x, y, _) = INPUT
+        .lines()
+        .fold((0, 0, 0), |(x, y, aim), line| {
+            let (insn, num) = line.split_at(line.find(' ').unwrap());
+            let num = num.trim().parse::<isize>().unwrap();
+            match insn {
+                "forward" => (x + num, y + aim * num, aim),
+                "down" => (x, y, aim + num),
+                "up" => (x, y, aim - num),
+                _ => panic!("invalid insn")
+            }
+        });
+
+    dbg!(x * y);
+}
